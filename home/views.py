0e0b6618114d04274f6f6ce36e0859b9c690f96c
ethan8621 <hope.86@live.cn>
2018-04-30 10:31:44 +0800

    init home page prototype

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+
+def home(request):
+    # for prototype
+    return HttpResponse("Hello, this is planner prototype.")
